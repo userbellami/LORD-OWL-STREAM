c5be216578910adbdee327d8b923bda1058cc1e0
userbellami <spikyhacking6@gmail.com>
2026-01-05 10:57:49 +0300

    Ultimate PWA: splash animation, install banner, maskable icon, offline caching, live site

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-self.addEventListener("fetch",()=>{});
+const CACHE_NAME = "lord-owl-stream-v1";
+const urlsToCache = ["/","/index.html","/manifest.json","/icon-192.png","/icon-512.png"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
